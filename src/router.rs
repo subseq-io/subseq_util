1155999c13123d32c9880e84db840035ce61d0fb
Teague Lasser <contact@teague.info>
2024-01-17 11:09:28 -0800

    Start documenting a little

--- src/router.rs
+++ src/router.rs
@@ -1,8 +1,12 @@
 use anymap::AnyMap;
 use tokio::sync::{broadcast, mpsc};
 
-pub const ROUTER_MESSAGE_SIZE: usize = 1024;
+pub const ROUTER_MESSAGE_LIMIT: usize = 1024;
 
+/// Stores handles to channels for better management of pipe ends.
+///
+/// Everything is typed by the struct being sent through the channel, so use
+/// custom types to clarify the rx/tx relationship.
 pub struct Router {
     broadcast_map: AnyMap,
     mpsc_map: AnyMap
@@ -21,14 +25,14 @@ impl Router {
         self.broadcast_map
             .entry::<broadcast::Sender<M>>()
             .or_insert_with(|| {
-                let (tx, _) = broadcast::channel(ROUTER_MESSAGE_SIZE);
+                let (tx, _) = broadcast::channel(ROUTER_MESSAGE_LIMIT);
                 tx
             })
             .clone()
     }
 
     pub fn create_channel<M: Send + Sync + 'static>(&mut self) -> mpsc::Receiver<M> {
-        let (tx, rx) = mpsc::channel(ROUTER_MESSAGE_SIZE);
+        let (tx, rx) = mpsc::channel(ROUTER_MESSAGE_LIMIT);
         self.mpsc_map.insert::<mpsc::Sender<M>>(tx);
         rx
     }
