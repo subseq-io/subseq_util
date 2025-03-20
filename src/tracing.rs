15e1d54cf6aa1b2f28fb05609dab2eef81a4b903
Teague Lasser <58143782+kraemahz@users.noreply.github.com>
2025-03-19 18:29:03 -0700

    Always run handler

--- src/tracing.rs
+++ src/tracing.rs
@@ -5,9 +5,9 @@ use tracing_subscriber::prelude::*;
 
 pub fn setup_tracing(app_name: &str, filter_level: Option<String>) {
     let default_log_levels = format!("{}=debug,subseq_util=debug", app_name);
+    LogTracer::init().expect("Failed to set logger");
     #[cfg(debug_assertions)]
     {
-        LogTracer::init().expect("Failed to set logger");
         let tracing_layer = tracing_subscriber::fmt::layer()
             .compact()
             .with_level(true)
