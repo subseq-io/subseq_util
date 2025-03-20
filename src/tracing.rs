64a02af39782c2d788671a76efc8bcfb265ccad9
Teague Lasser <58143782+kraemahz@users.noreply.github.com>
2025-03-19 18:19:29 -0700

    Add logging info

--- src/tracing.rs
+++ src/tracing.rs
@@ -1,9 +1,10 @@
+use crate::SUBSEQ_UTIL_VERSION;
 use tracing_log::LogTracer;
 use tracing_subscriber::filter::EnvFilter;
 use tracing_subscriber::prelude::*;
-use crate::SUBSEQ_UTIL_VERSION;
 
 pub fn setup_tracing(app_name: &str, filter_level: Option<String>) {
+    let default_log_levels = format!("{}=debug,subseq_util=debug", app_name);
     #[cfg(debug_assertions)]
     {
         LogTracer::init().expect("Failed to set logger");
@@ -20,21 +21,20 @@ pub fn setup_tracing(app_name: &str, filter_level: Option<String>) {
             let filter_layer = if let Some(filter_level) = filter_level.clone() {
                 EnvFilter::new(filter_level)
             } else {
-                EnvFilter::new(format!("{}=debug,subseq_util=debug", app_name))
+                EnvFilter::new(default_log_levels.as_str())
             };
             tracing_subscriber::registry()
                 .with(filter_layer)
                 .with(console_layer)
                 .with(tracing_layer)
                 .init();
-            tracing::info!("Tracing started with console: {:?}", filter_level);
         }
         #[cfg(not(feature = "console"))]
         {
             let filter_layer = if let Some(filter_level) = filter_level.clone() {
                 EnvFilter::new(filter_level)
             } else {
-                EnvFilter::new(format!("{}=debug,subseq_util=debug", app_name))
+                EnvFilter::new(default_log_levels.as_str())
             };
             tracing_subscriber::registry()
                 .with(filter_layer)
@@ -49,12 +49,24 @@ pub fn setup_tracing(app_name: &str, filter_level: Option<String>) {
         let filter_layer = if let Some(filter_level) = filter_level {
             EnvFilter::new(filter_level)
         } else {
-            EnvFilter::new(format!("{}=info,subseq_util=info", app_name))
+            EnvFilter::new(default_log_levels.as_str())
         };
         tracing_subscriber::registry()
             .with(filter_layer)
             .with(tracing_layer)
             .init();
     }
-    tracing::info!("App '{}' started (subseq_util: {})", app_name, SUBSEQ_UTIL_VERSION);
+    tracing::debug!(
+        "Tracing started: {}",
+        if let Some(filter_level) = filter_level {
+            filter_level
+        } else {
+            default_log_levels
+        }
+    );
+    tracing::info!(
+        "App '{}' started (subseq_util: {})",
+        app_name,
+        SUBSEQ_UTIL_VERSION
+    );
 }
